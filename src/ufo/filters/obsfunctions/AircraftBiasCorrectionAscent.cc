9618be20bc74c01b18c2d24eb96979377193cf9a
BrettHoover-NOAA <Brett.Hoover@noaa.gov>
2023-05-25 13:36:24 -0400

    Added obsfunctions/AircraftBiasCorrectionAscent(.cc,.h) and ...AscentSquared(.cc,.h), and registered in CMakeLists.txt

---
+++ src/ufo/filters/obsfunctions/AircraftBiasCorrectionAscent.cc
@@ -0,0 +1,93 @@
+/*
+ * (C) Copyright 2023 NOAA/NWS/NCEP/EMC
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "ufo/filters/obsfunctions/AircraftBiasCorrectionAscent.h"
+
+#include <algorithm>
+#include <cmath>
+#include <valarray>
+#include <vector>
+
+#include "ioda/ObsDataVector.h"
+#include "oops/util/missingValues.h"
+#include "ufo/filters/ObsFilterData.h"
+#include "ufo/filters/Variable.h"
+#include "ufo/utils/Constants.h"
+
+namespace ufo {
+
+static ObsFunctionMaker<AircraftBiasCorrectionAscent> makerObsFuncAircraftBiasCorrectionAscent_("AircraftBiasCorrectionAscent");
+
+// -----------------------------------------------------------------------------
+
+AircraftBiasCorrectionAscent::AircraftBiasCorrectionAscent(const eckit::LocalConfiguration & conf)
+  : invars_() {
+  oops::Log::debug() << "AircraftBiasCorrectionAscent: config = " << conf << std::endl;
+  // Initialize options
+  options_.deserialize(conf);
+
+  // We need to retrieve the aircraft bias correction ascent coefficient
+  invars_ += Variable("BiasCoefficientValue/ascentPredictor");
+  // We need to retrieve the aircraft ascent rate
+  invars_ += Variable("MetaData/windUpward");
+  // Typical use would be HofX group, but during testing, we include option for GsiHofX
+  //BTH//std::string test_hofx = options_.test_hofx.value();
+  //BTH//invars_ += Variable(test_hofx + "/windEastward");
+  //BTH//invars_ += Variable(test_hofx + "/windNorthward");
+
+  // TODO(gthompsn): Need to include a check that whatever HofX group name used actually exists.
+}
+
+// -----------------------------------------------------------------------------
+
+AircraftBiasCorrectionAscent::~AircraftBiasCorrectionAscent() {}
+
+// -----------------------------------------------------------------------------
+
+void AircraftBiasCorrectionAscent::compute(const ObsFilterData & in,
+                                  ioda::ObsDataVector<float> & out) const {
+  const size_t nlocs = in.nlocs();
+  const float missing = util::missingValue(missing);
+  //BTH//const double deg = Constants::rad2deg;
+
+  // Ensure that only one output variable is expected.
+  ASSERT(out.nvars() == 1);
+
+  // Retrieve minimum_uv value and assure it is sensible.
+  //BTH//const float min_uv = std::max(0.0001f, options_.minimum_uv.value());
+
+  // Retrieve observation aircraft bias correction constant coefficient
+  std::vector<float> BCcoeff;
+  in.get(Variable("BiasCoefficientValue/constantPredictor"), BCcoeff);
+  // Retrieve observation aircraft ascent rate
+  std::vector<float> BCpredi;
+  in.get(Variable("MetaData/windUpward"), BCpredi);
+  // Retrieve Model HofX wind components
+  //BTH//std::string test_hofx = options_.test_hofx.value();
+  //BTH//std::vector<float> um, vm;
+  //BTH//in.get(Variable(test_hofx + "/windEastward"), um);
+  //BTH//in.get(Variable(test_hofx + "/windNorthward"), vm);
+
+  for (size_t jj = 0; jj < nlocs; ++jj) {
+    if (BCcoeff[jj] != missing & BCpredi[jj] != missing) {
+      // ascent predictor = BCpredi*BCcoeff
+      out[0][jj] = BCpredi[jj]*BCcoeff[jj];
+    } else {
+      out[0][jj] = missing;
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+
+const ufo::Variables & AircraftBiasCorrectionAscent::requiredVariables() const {
+  return invars_;
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
---
+++ src/ufo/filters/obsfunctions/AircraftBiasCorrectionAscent.h
@@ -0,0 +1,72 @@
+/*
+ * (C) Copyright 2023 NOAA/NWS/NCEP/EMC
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#ifndef UFO_FILTERS_OBSFUNCTIONS_AIRCRAFTBIASCORRECTIONASCENT_H_
+#define UFO_FILTERS_OBSFUNCTIONS_AIRCRAFTBIASCORRECTIONASCENT_H_
+
+#include <string>
+
+#include "oops/util/parameters/Parameter.h"
+
+#include "ufo/filters/obsfunctions/ObsFunctionBase.h"
+#include "ufo/filters/Variables.h"
+
+namespace ufo {
+//BTH//
+///
+/// \brief An optional parameter to override the source of HofX wind components,
+///        and an optional parameter for minimum wind components (default=0.5 m/s).
+///
+class AircraftBiasCorrectionAscentParameters : public oops::Parameters {
+  OOPS_CONCRETE_PARAMETERS(AircraftBiasCorrectionAscentParameters, Parameters)
+//BTH//
+//BTH// public:
+//BTH//  /// Name of the HofX group used to replace the default group (default is HofX)
+//BTH//  oops::Parameter<std::string> test_hofx{"test_hofx", "HofX", this};
+//BTH//  oops::Parameter<float> minimum_uv{"minimum_uv", 0.5, this};
+};
+
+// -----------------------------------------------------------------------------
+
+/// \brief Compute the ascent predictor for aircraft bias correction, which is
+///        equal to <ascent-rate> * <bias correction coefficient>. This can be
+///        combined with the aircraft airTemperature and other predictors through
+///        use of a LinearCombination filter to bias correct the airTemperature
+///        observations.
+///
+/// ~~~
+///
+/// ### Sample YAML configuration
+///     - filter: Variable Assignment
+///       filter variables:
+///       - name: airTemperature
+///       assignments:
+///       - name: BiasCorrectionPredictor/ascentPredictor
+///         type: float
+///       function:
+///         name: ObsFunction/AircraftBiasCorrectionAscent
+///
+class AircraftBiasCorrectionAscent : public ObsFunctionBase<float> {
+ public:
+  static const std::string classname() {return "AircraftBiasCorrectionAscent";}
+
+  explicit AircraftBiasCorrectionAscent(const eckit::LocalConfiguration &
+                                 = eckit::LocalConfiguration());
+  ~AircraftBiasCorrectionAscent();
+
+  void compute(const ObsFilterData &, ioda::ObsDataVector<float> &) const;
+  const ufo::Variables & requiredVariables() const;
+ private:
+  ufo::Variables invars_;
+  AircraftBiasCorrectionAscent options_;
+};
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
+
+#endif  // UFO_FILTERS_OBSFUNCTIONS_AIRCRAFTBIASCORRECTIONASCENT_H_
